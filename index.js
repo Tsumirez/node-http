49232e82536f27c64f7d379ac18e840b62e34d6a
Tsumirez <tsumirez@zoho.com>
2022-04-05 00:40:32 +0200

    Added 404 page not found routing.

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   if (req.url === "/MC") {
-    res.writeHead(200, {
-      "Content-Type": "application/json",
-    });
+    // res.writeHead(200, {
+    //   "Content-Type": "application/json",
+    // });
+
+    //same functionality as commented code, just different calls.
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+
     res.end(
       JSON.stringify({
         name: "Nick Slaughter",
         type: "private detective",
       })
     );
-  } else {
+  } else if (req.url==='/') {
     res.writeHead(200, {
         "Content-Type": "text/html",
     });
@@ -26,13 +31,10 @@ const server = http.createServer((req, res) => {
     res.write("<h2>Main characters</h2>");
     res.write("</body>");
     res.write("</html>");
-    res
-      .end
-      //   JSON.stringify({
-      //     name: "Key-meriah",
-      //     type: "island",
-      //   })
-      ();
+    res.end();
+  } else {
+      res.statusCode = 404;
+      res.end();
   }
 });
 
