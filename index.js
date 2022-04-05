3f2259451ec11c4e1607e263243db9de8e370909
Tsumirez <tsumirez@zoho.com>
2022-04-05 11:16:24 +0200

    Added parsing of url parameters. primitive and error prone, but functional

--- index.js
+++ index.js
@@ -2,25 +2,54 @@ const http = require("http");
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/MC") {
+const cast = [
+  {
+    id: 0,
+    actor: "Rob Steward",
+    character: "Nick Slaughter",
+  },
+  {
+    id: 1,
+    actor: "Carolyn Dunn",
+    character: "Sylvie Girard",
+  },
+  {
+    id: 2,
+    actor: "Ian Tracey",
+    character: "Spider Garvin",
+  },
+  {
+    id: 3,
+    actor: "Ari Sorco-Ram",
+    character: "",
+  },
+  {
+    id: 4,
+    actor: 'Eugene Clark',
+    character:'Ollie Porter'
+  }
+];
+
+const server = http.createServer();
+
+server.on('request', (req,res) => {
+  const items = req.url.split('/');
+  if (items.length == 2) {
     // res.writeHead(200, {
     //   "Content-Type": "application/json",
     // });
 
     //same functionality as commented code, just different calls.
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
 
     res.end(
-      JSON.stringify({
-        name: "Nick Slaughter",
-        type: "private detective",
-      })
+      JSON.stringify(cast)
     );
-  } else if (req.url==='/') {
+  } else if (items.length === 3) {
+    let movieCharacter = +items[2];
     res.writeHead(200, {
-        "Content-Type": "text/html",
+      "Content-Type": "text/html",
     });
     res.write("<html>");
     res.write("<head>");
@@ -28,13 +57,14 @@ const server = http.createServer((req, res) => {
     res.write("</head>");
     res.write("<body>");
     res.write("<h1>Tropical Heat TV Series</h1>");
-    res.write("<h2>Main characters</h2>");
+    res.write(`<h2>${cast[movieCharacter].character}</h2>`);
+    res.write(`<h2>Played by: ${cast[movieCharacter].actor}</h2>`);
     res.write("</body>");
     res.write("</html>");
     res.end();
   } else {
-      res.statusCode = 404;
-      res.end();
+    res.statusCode = 404;
+    res.end();
   }
 });
 
